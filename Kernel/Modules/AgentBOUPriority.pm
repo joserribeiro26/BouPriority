b4a9728c57b9bade6d2b26da2f0f77678dd04801
junior <jjunior@beonup.com.br>
2016-06-10 23:56:01 -0300

    update

---
+++ Kernel/Modules/AgentBOUPriority.pm
@@ -0,0 +1,77 @@
+# --
+# Copyright (C) 2015-2016 BeOnUp http://www.beonup.com.br
+# --
+
+package Kernel::Modules::AgentBOUPriority;
+
+use strict;
+use warnings;
+
+use Kernel::System::EmailParser;
+use Kernel::System::VariableCheck qw(:all);
+use Data::Dumper;
+
+our $ObjectManagerDisabled = 1;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ParamObject  = $Kernel::OM->Get('Kernel::System::Web::Request');
+
+    my %GetParam;
+    for my $Key (
+        qw(
+        NewStateID NewPriorityID TimeUnits ArticleTypeID Title Body Subject NewQueueID
+        Year Month Day Hour Minute NewOwnerID NewResponsibleID TypeID ServiceID SLAID
+        Expand ReplyToArticle StandardTemplateID CreateArticle
+        )
+        )
+		{
+			$GetParam{$Key} = $ParamObject->GetParam( Param => $Key );
+		}
+			
+			my $Success1 = $TicketObject->TicketTypeSet(
+			TypeID   => $GetParam{TypeID},
+			TicketID  => $Self->{TicketID},
+			UserID    => $Self->{UserID},    );
+
+			my $Success2 = $TicketObject->TicketServiceSet(
+				ServiceID => $GetParam{ServiceID},
+				TicketID  => $Self->{TicketID},
+				UserID    => $Self->{UserID},
+			);
+			my $Success3 = $TicketObject->TicketSLASet(
+				SLAID    => $GetParam{SLAID},
+				TicketID  => $Self->{TicketID},
+				UserID    => $Self->{UserID},
+			);
+	
+
+			#   my $Success = $TicketObject->TicketPrioritySet(
+  #     TicketID   => $GetParam{SLAID},
+  #     TicketID  => $Self->{TicketID},
+  #     UserID    => $Self->{UserID},
+  # );
+	
+	
+	
+	  my $HTML = $LayoutObject->Redirect(
+        OP => "Action=AgentTicketZoom;TicketID=$Self->{TicketID}",
+    );
+	
+	return $HTML;
+      
+}
---
+++ Kernel/Output/HTML/OutputFilter/AgentBOUPriority.pm
@@ -0,0 +1,407 @@
+
+package Kernel::Output::HTML::OutputFilter::AgentBOUPriority;
+
+use strict;
+use warnings;
+
+use List::Util qw(first);
+use Kernel::System::EmailParser;
+use Kernel::System::VariableCheck qw(:all);
+
+our @ObjectDependencies = qw(
+    Kernel::Config
+    Kernel::System::Encode
+    Kernel::System::Log
+    Kernel::System::Main
+    Kernel::System::DB
+    Kernel::System::Time
+    Kernel::System::Web::Request
+    Kernel::Output::HTML::Layout
+);
+
+sub new {
+my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+   return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get needed objects
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ParamObject  = $Kernel::OM->Get('Kernel::System::Web::Request');
+
+	my %Ticket = $TicketObject->TicketGet(
+        TicketID      => $Self->{TicketID},
+        DynamicFields => 1,
+        UserID        => $Self->{UserID},
+        Silent        => 1, 
+    );
+
+    # get config of frontend module
+    my $Config = $ConfigObject->Get("Ticket::Frontend::AgentTicketNote");
+
+
+
+    # get ACL restrictions
+    my %PossibleActions = ( 1 => $Self->{Action} );
+
+    my $ACL = $TicketObject->TicketAcl(
+        Data          => \%PossibleActions,
+        Action        => $Self->{Action},
+        TicketID      => $Self->{TicketID},
+        ReturnType    => 'Action',
+        ReturnSubType => '-',
+        UserID        => $Self->{UserID},
+    );
+    my %AclAction = $TicketObject->TicketAclActionData();
+
+    my %GetParam;
+    for my $Key (
+        qw(
+        NewStateID NewPriorityID TimeUnits ArticleTypeID Title Subject NewQueueID
+        Year Month Day Hour Minute NewOwnerID NewResponsibleID TypeID ServiceID SLAID
+        Expand ReplyToArticle StandardTemplateID CreateArticle CustomerUser
+        )
+        )
+    {
+        $GetParam{$Key} = $ParamObject->GetParam( Param => $Key );
+    }
+
+	
+
+    # get dynamic field values form http request
+    my %DynamicFieldValues;
+
+    # to store the reference to the dynamic field for the impact
+
+    # define the dynamic fields to show based on the object type
+    my $ObjectType = ['Ticket'];
+
+    # only screens that add notes can modify Article dynamic fields
+    if ( $Config->{Note} ) {
+        $ObjectType = [ 'Ticket', 'Article' ];
+    }
+
+    my %DynamicFieldHTML;
+
+    my $Output;
+	my %Data;
+
+
+		if ( $ConfigObject->Get('AgentBOUPriority::Type') ){
+		
+			my $Types = $Self->_GetTypes(
+				%GetParam,
+				TicketID   =>  $Self->{TicketID},
+				
+			);
+			
+		    $Data{TypeStrg} = $LayoutObject->BuildSelection(
+				Data        => $Types,
+				Name        => 'TypeID',
+				SelectedID  => $Ticket{TypeID},
+				Size        => 5,
+				Multiple    => 0,
+				TreeView    => 1,
+				Translation => 0,
+				Max         => 200,
+				Class       => 'Modernize',
+				OnChange	=> "this.form.submit();"
+
+			);
+	        $LayoutObject->Block(
+				Name => 'Type',
+				Data => {%Data},
+			);
+		}
+		
+	# if ( $ConfigObject->Get('AgentBOUPriority::Priority') ){
+    # 
+	# 	my $Priorities = $Self->_GetPriorities(
+    #         %GetParam,
+    #     );
+	# 		$Data{PriorityStrg} = $LayoutObject->BuildSelection(
+	# 			Data        => $Priorities,
+	# 			Name        => 'PriorityID',
+	# 			SelectedID  => $Ticket{PriorityID},
+	# 			Size        => 5,
+	# 			Multiple    => 0,
+	# 			TreeView    => 1,
+	# 			Translation => 0,
+	# 			Max         => 200,
+	# 			Class       => 'Modernize',
+	# 			OnChange	=> "this.form.submit();"
+    # 
+	# 		);
+	#         $LayoutObject->Block(
+	# 			Name => 'Priority',
+	# 			Data => {%Data},
+	# 		);
+	# 	
+	# 	
+	# }		
+	
+		
+		if ( $ConfigObject->Get('AgentBOUPriority::Service') ){
+			
+			my $Services = $Self->_GetServices(
+            %GetParam,
+			TicketID => $Self->{TicketID},
+			);
+			
+		    my $SLAs = $Self->_GetSLAs(
+				%GetParam,
+				QueueID      => $Ticket{QueueID},
+				ServiceID      => $Ticket{ServiceID},
+			);
+			
+		
+			$Data{ServiceSrt} = $LayoutObject->BuildSelection(
+				Data        => $Services,
+				Name        => 'ServiceID',
+				SelectedID  => $Ticket{ServiceID},
+				Size        => 5,
+				Multiple    => 0,
+				TreeView    => 1,
+				Translation => 0,
+				Max         => 200,
+				Class       => 'Modernize',
+				OnChange	=> "this.form.submit();"
+
+			);
+			
+			$Data{SLAStrg} .= $LayoutObject->BuildSelection(
+				Data        => $SLAs,
+				Name        => 'SLAID',
+				SelectedID  => $Ticket{SLAID},
+				Size        => 5,
+				Multiple    => 0,
+				TreeView    => 1,
+				Translation => 0,
+				Max         => 200,
+				Class       => 'Modernize',
+				OnChange	=> "this.form.submit();"
+
+			);
+			
+			$LayoutObject->Block(
+                Name => 'Service',
+                Data => {%Data},
+            );
+			
+			$LayoutObject->Block(
+                Name => 'SLA',
+                Data => {%Data},
+            );
+
+		}
+			
+
+		# 
+		
+		my $iFrame = $LayoutObject->Output(
+    	    TemplateFile => 'AgentBOUPriority',
+	        Data         => \%Data,
+    	); 
+	    ${ $Param{Data} } =~ s{(<div \s+ id="ArticleTree">)}{$iFrame $1}xms;
+		
+    return ${ $Param{Data} };
+	
+}
+
+
+sub _GetNextStates {
+    my ( $Self, %Param ) = @_;
+
+    my %NextStates = $Kernel::OM->Get('Kernel::System::Ticket')->TicketStateList(
+        TicketID => $Self->{TicketID},
+        Action   => $Self->{Action},
+        UserID   => $Self->{UserID},
+        %Param,
+    );
+
+    return \%NextStates;
+}
+
+sub _GetResponsible {
+    my ( $Self, %Param ) = @_;
+    my %ShownUsers;
+    my %AllGroupsMembers = $Kernel::OM->Get('Kernel::System::User')->UserList(
+        Type  => 'Long',
+        Valid => 1,
+    );
+
+    # show all users
+    if ( $Kernel::OM->Get('Kernel::Config')->Get('Ticket::ChangeOwnerToEveryone') ) {
+        %ShownUsers = %AllGroupsMembers;
+    }
+
+    # show only users with responsible or rw pemissions in the queue
+    elsif ( $Param{QueueID} && !$Param{AllUsers} ) {
+        my $GID = $Kernel::OM->Get('Kernel::System::Queue')->GetQueueGroupID(
+            QueueID => $Param{NewQueueID} || $Param{QueueID}
+        );
+        my %MemberList = $Kernel::OM->Get('Kernel::System::Group')->PermissionGroupGet(
+            GroupID => $GID,
+            Type    => 'responsible',
+        );
+        for my $UserID ( sort keys %MemberList ) {
+            $ShownUsers{$UserID} = $AllGroupsMembers{$UserID};
+        }
+    }
+
+    # get ticket object
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    # workflow
+    my $ACL = $TicketObject->TicketAcl(
+        %Param,
+        Action        => $Self->{Action},
+        ReturnType    => 'Ticket',
+        ReturnSubType => 'Responsible',
+        Data          => \%ShownUsers,
+        UserID        => $Self->{UserID},
+    );
+
+    return { $TicketObject->TicketAclData() } if $ACL;
+
+    return \%ShownUsers;
+}
+
+sub _GetServices {
+    my ( $Self, %Param ) = @_;
+
+    # get service
+    my %Service;
+
+    # get options for default services for unknown customers
+    my $DefaultServiceUnknownCustomer
+        = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Service::Default::UnknownCustomer');
+
+    # check if no CustomerUserID is selected
+    # if $DefaultServiceUnknownCustomer = 0 leave CustomerUserID empty, it will not get any services
+    # if $DefaultServiceUnknownCustomer = 1 set CustomerUserID to get default services
+    if ( !$Param{CustomerUserID} && $DefaultServiceUnknownCustomer ) {
+        $Param{CustomerUserID} = '<DEFAULT>';
+    }
+
+    # get service list
+    if ( $Param{CustomerUserID} ) {
+        %Service = $Kernel::OM->Get('Kernel::System::Ticket')->TicketServiceList(
+            %Param,
+            Action => $Self->{Action},
+            UserID => $Self->{UserID},
+        );
+    }
+    return \%Service;
+}
+
+sub _GetSLAs {
+    my ( $Self, %Param ) = @_;
+
+    # if non set customers can get default services then they should also be able to get the SLAs
+    #  for those services (this works during ticket creation).
+    # if no CustomerUserID is set, TicketSLAList will complain during AJAX updates as UserID is not
+    #  passed. See bug 11147.
+
+    # get options for default services for unknown customers
+    my $DefaultServiceUnknownCustomer
+        = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Service::Default::UnknownCustomer');
+
+    # check if no CustomerUserID is selected
+    # if $DefaultServiceUnknownCustomer = 0 leave CustomerUserID empty, it will not get any services
+    # if $DefaultServiceUnknownCustomer = 1 set CustomerUserID to get default services
+    if ( !$Param{CustomerUserID} && $DefaultServiceUnknownCustomer ) {
+        $Param{CustomerUserID} = '<DEFAULT>';
+    }
+
+    my %SLA;
+    if ( $Param{ServiceID} ) {
+        %SLA = $Kernel::OM->Get('Kernel::System::Ticket')->TicketSLAList(
+            %Param,
+            Action => $Self->{Action},
+        );
+    }
+    return \%SLA;
+}
+
+sub _GetPriorities {
+    my ( $Self, %Param ) = @_;
+
+    my %Priorities = $Kernel::OM->Get('Kernel::System::Ticket')->TicketPriorityList(
+        %Param,
+        Action   => $Self->{Action},
+        UserID   => $Self->{UserID},
+        TicketID => $Self->{TicketID},
+    );
+
+    # get config of frontend module
+    my $Config = $Kernel::OM->Get('Kernel::Config')->Get("Ticket::Frontend::$Self->{Action}");
+
+    if ( !$Config->{PriorityDefault} ) {
+        $Priorities{''} = '-';
+    }
+    return \%Priorities;
+}
+
+sub _GetFieldsToUpdate {
+    my ( $Self, %Param ) = @_;
+
+    my @UpdatableFields;
+
+    # set the fields that can be updateable via AJAXUpdate
+    if ( !$Param{OnlyDynamicFields} ) {
+        @UpdatableFields = qw(
+            TypeID ServiceID SLAID NewOwnerID NewResponsibleID NewStateID
+            NewPriorityID
+        );
+    }
+
+    # define the dynamic fields to show based on the object type
+    my $ObjectType = ['Ticket'];
+
+    # get config of frontend module
+    my $Config = $Kernel::OM->Get('Kernel::Config')->Get("Ticket::Frontend::$Self->{Action}");
+
+    # only screens that add notes can modify Article dynamic fields
+    if ( $Config->{Note} ) {
+        $ObjectType = [ 'Ticket', 'Article' ];
+    }
+
+    # get the dynamic fields for this screen
+    my $DynamicField = $Kernel::OM->Get('Kernel::System::DynamicField')->DynamicFieldListGet(
+        Valid       => 1,
+        ObjectType  => $ObjectType,
+        FieldFilter => $Config->{DynamicField} || {},
+    );
+
+
+
+    return \@UpdatableFields;
+}
+
+
+sub _GetTypes {
+    my ( $Self, %Param ) = @_;
+
+    # get type
+    my %Type;
+    if ( $Param{QueueID} || $Param{TicketID} ) {
+        %Type = $Kernel::OM->Get('Kernel::System::Ticket')->TicketTypeList(
+            %Param,
+            Action => $Self->{Action},
+            UserID => $Self->{UserID},
+        );
+    }
+    return \%Type;
+}
+
+1;
